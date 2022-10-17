065ac51523454605e6a6924d03b65e010d94bbdd
Titus Mumo <titusmumo@gmail.com>
2022-10-17 19:43:33 +0300

    Completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,32 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+
+def greet (name)
+    puts "Hello, #{name}!"
+end
+greet("Naureen")
+
+
+def greet_with_default (name="programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default("Naureen")
+
+
+def add (num1, num2)
+    return num1 + num2
+end
+add(1,2)
+
+def halve (number)
+    if (number.class != Integer)
+        return nil
+    end
+
+    number /2 
+end
+halve("six")
